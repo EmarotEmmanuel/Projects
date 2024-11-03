701ed97db8c4fd37dead4bdacebf70d060b593c3
Emarot Emmanuel <emarotemmanuel@gmail.com>
2024-11-03 16:16:56 +0300

    Edited Version

--- gps_tracker/settings.py
+++ gps_tracker/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'gps_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
